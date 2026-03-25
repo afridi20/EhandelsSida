866759294d28e60e8619f277367142f029f68054
Habil khan <habkha905@gmail.com>
2026-03-25 10:10:21 +0100

    erros fixed

---
+++ Ecomerce/EhandelsSida/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿namespace EhandelsSida.Models
+{
+    public class Order
+    {
+        public int id { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; } =new List<OrderItem>();
+
+            public decimal TotalAmount
+        {
+
+            get
+            {
+                decimal total = 0;
+                foreach (var item in OrderItems)
+                {
+                    total += item.Quantity * item.UnitPrice;
+                }
+                return total;
+            }
+        }
+
+             
+      
+    }
+}
